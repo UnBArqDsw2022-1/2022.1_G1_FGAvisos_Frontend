99a149220122042df28725114e23ae02601ec8e5
victorhugo21 <hugo.victorcsi@gmail.com>
2022-08-20 17:12:35 -0300

    Adicionando componente de input
    Co-Authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

---
+++ app/src/components/Input/styles.ts
@@ -0,0 +1,26 @@
+
+import styled from 'styled-components';
+
+interface InputProps{
+    backgroundColor: string;
+    width: number;
+    colorFont: string;
+}
+export const InputStyle = styled.input<InputProps>`
+    background-color: ${props => props.backgroundColor};
+    width: ${props => props.width}px;
+    border: 0.5px ;
+    border-radius: 5px;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 50%);
+    margin: 15px 0;
+    height: inherit;
+    padding: 7px 0px 7px 10px;
+    font-size: 15px;
+    color: ${props => props.colorFont};;
+    &::-webkit-input-placeholder {
+      color: ${props => props.colorFont};;
+    }
+`;
+
+
+
