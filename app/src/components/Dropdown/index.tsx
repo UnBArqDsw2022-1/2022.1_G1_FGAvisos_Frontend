51db660466e585fe62619ac06b8422f0dd2cc35f
vitordiniz25 <vitordiniz25@gmail.com>
2022-08-20 19:42:09 -0300

    criando estrutura inicial do dropdown

---
+++ app/src/components/Dropdown/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Dropdown = () => {
+  return (
+    <form action="">
+      <select name="dropdown">
+        <option value="turma1">Turma1</option>
+        <option value="turma2">Turma2</option>
+        <option value="turma3">Turma3</option>
+        <option value="turma4">Turma4</option>
+      </select>
+    </form>
+  );
+};
---
+++ app/src/components/Dropdown/styles.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const DropdownStyled = styled.select``;
