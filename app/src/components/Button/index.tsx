5a7bede6724e7cfcc4600bdcca058419ea1f7352
victorhugo21 <hugo.victorcsi@gmail.com>
2022-08-20 23:11:43 -0300

    Adicionando componente de botão generico
    Co-Authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

---
+++ app/src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { ButtonStyle } from './styles';
+
+interface ButtonProps {
+    text: string;
+    backgroundColor: string;
+    colorFont: string;
+    width: number;
+}
+
+const Button: React.FC<ButtonProps> = ({...props}) => 
+   (<ButtonStyle
+        {...props}>{props.text}
+    </ButtonStyle>);
+
+export default Button;
