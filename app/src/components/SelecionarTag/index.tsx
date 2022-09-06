20a2d2c44a63651242413e967ff7d5a0cb49a71a
antonio.igor <antonioigorcarvalho@gmail.com>
2022-09-05 21:42:14 -0300

    Base para seleção de tags criada

--- app/src/components/SelecionarTag/index.tsx
+++ app/src/components/SelecionarTag/index.tsx
@@ -9,43 +9,57 @@ import * as Styles from "./styles.js";
 const SelecionarTag: FC<any> = ({ ...props }) => {
   return (
     <>
-      <Styles.TagsContainer>
-        <Button
-          text={"MUITO URGENTE"}
-          backgroundColor={"black"}
-          colorFont={"white"}
-          width={140}
-          height={30}
-        ></Button>
-        <Button
-          text={"URGENTE"}
-          backgroundColor={"red"}
-          colorFont={"white"}
-          width={140}
-          height={30}
-        ></Button>
-        <Button
-          text={"IMPORTANTE"}
-          backgroundColor={"orange"}
-          colorFont={"white"}
-          width={140}
-          height={30}
-        ></Button>
-        <Button
-          text={"AVISO"}
-          backgroundColor={"green"}
-          colorFont={"white"}
-          width={140}
-          height={30}
-        ></Button>
-        <Button
-          text={"PROVA"}
-          backgroundColor={"yellow"}
-          colorFont={"black"}
-          width={140}
-          height={30}
-        ></Button>
-      </Styles.TagsContainer>
+      <Styles.AbrirTags>
+
+      </Styles.AbrirTags>
+      <Styles.Container>
+        <TituloGenerico
+          titulo={"Defina as tags:"}
+          tamanho={Tema.fonte.tamanhos.media}
+          cor={"white"}
+        ></TituloGenerico>
+        <Styles.TagsContainer>
+          <Button
+            text={"MUITO URGENTE"}
+            backgroundColor={"black"}
+            colorFont={"white"}
+            width={140}
+            height={30}
+          ></Button>
+          <Styles.Divider></Styles.Divider>
+          <Button
+            text={"URGENTE"}
+            backgroundColor={"red"}
+            colorFont={"white"}
+            width={140}
+            height={30}
+          ></Button>
+          <Styles.Divider></Styles.Divider>
+          <Button
+            text={"IMPORTANTE"}
+            backgroundColor={"orange"}
+            colorFont={"white"}
+            width={140}
+            height={30}
+          ></Button>
+          <Styles.Divider></Styles.Divider>
+          <Button
+            text={"AVISO"}
+            backgroundColor={"green"}
+            colorFont={"white"}
+            width={140}
+            height={30}
+          ></Button>
+          <Styles.Divider></Styles.Divider>
+          <Button
+            text={"PROVA"}
+            backgroundColor={"yellow"}
+            colorFont={"black"}
+            width={140}
+            height={30}
+          ></Button>
+        </Styles.TagsContainer>
+      </Styles.Container>
     </>
   );
 };
--- app/src/components/SelecionarTag/styles.js
+++ app/src/components/SelecionarTag/styles.js
@@ -1,7 +1,21 @@
 import { Tema } from "../../utils/Tema";
 import styled, { css } from "styled-components";
 
+const AbrirTags = styled.div`
+  background-color: gray;
+`;
+
+const Container = styled.div`
+  background-color: gray;
+  padding: 1rem;
+  width: 200px;
+  align-items: center;
+  border: 0.5px solid black;
+  border-radius: 7px;
+`;
+
 const TagsContainer = styled.div`
+    align-items: center;
     display: flex;
     flex-direction: column;
     
@@ -10,4 +24,10 @@ const TagsContainer = styled.div`
     }
 `;
 
-export { TagsContainer };
+const Divider = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: black;
+`;
+
+export { AbrirTags, Container, TagsContainer, Divider };
