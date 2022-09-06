f83b1ac6fc8305c12c14037ce628fc2db06c82dc
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-09-05 23:51:01 -0300

    Adicionando Componente Área de Texto

---
+++ app/src/components/AreaTexto/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AreaTextoEstilo } from './styles';
+
+export default function AreaTexto (){
+
+    return(
+
+        <AreaTextoEstilo placeholder='Escreva aqui seu aviso'></AreaTextoEstilo>
+
+    )
+}
