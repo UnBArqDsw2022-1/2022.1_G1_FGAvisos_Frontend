743a83fe37de550d58e750d748fc8ccc48e17d92
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-09-07 12:52:08 -0300

    Adicionando página de login
    Co-Authored-By: João Paulo Coelho <40813329+JoaoP-Coelho@users.noreply.github.com>

---
+++ app/src/pages/PaginaLogin/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Corpo, CaixaLogin, Conteudo, CampoUsuario, CampoSenha, BotaoConfirmar} from './styles';
+import Footer from '../../components/Footer';
+import BarraNavegacao from '../../components/BarraNavegacao';
+import Input from '../../components/Input';
+import Tema from '../../utils/Tema';
+import Button from '../../components/Button';
+
+
+function PaginaLogin({...props}){
+
+
+
+    return(
+
+        <Corpo>
+            <BarraNavegacao></BarraNavegacao>
+
+            <Conteudo>
+                <CaixaLogin>
+
+                    <CampoUsuario>
+                        <Input 
+                            text={"Usuário/email"} 
+                            backgroundColor={Tema.colors.$secundariaClara} 
+                            colorFont={"white"}
+                            width={"493px"}
+                        ></Input>
+                    </CampoUsuario>
+
+                    
+                    <CampoSenha>
+                        <Input
+                            text={"Senha"}
+                            backgroundColor={Tema.colors.$secundariaClara}
+                            colorFont={"white"}
+                            width={"493px"}
+                            type={"password"}
+                        ></Input>
+                    </CampoSenha>
+                    
+                    <div>
+                        <a href={props.esqueciSenha}>Esqueci a senha?</a><br/><br/>
+                    </div>
+
+                    <BotaoConfirmar>
+                        <Button
+                            text={"Confirmar"}
+                            backgroundColor={Tema.colors.$secundariaClara}
+                            colorFont={"white"}
+                            width = {203}
+                        ></Button>
+                    </BotaoConfirmar>
+                    
+                </CaixaLogin>
+            </Conteudo>
+    
+            <Footer></Footer>
+        </Corpo>
+
+    );
+}
+
+PaginaLogin.propTypes = {
+    esqueciSenha : String
+}
+
+PaginaLogin.defaultProps = {
+    esqueciSenha: "google.com"
+}
+
+export default PaginaLogin;
