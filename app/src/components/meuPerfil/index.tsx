66150f7088c4930710b8993ee37b327e4b2cf4b4
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-09-07 17:22:50 -0300

    Adaptando o componente MeuPerfil
    Co-Authored-By: João Paulo Coelho <40813329+JoaoP-Coelho@users.noreply.github.com>

--- app/src/components/meuPerfil/index.tsx
+++ app/src/components/meuPerfil/index.tsx
@@ -4,41 +4,36 @@ import {ConteinerMaior, Perfil, TextoEscolha, Editar, IconePerfil} from  "./styl
 import { Tema } from "../../utils/Tema";
 
 
+interface MeuPerfilProps{
+    editar: boolean;
+}
 
 
+const MeuPerfil: FC<MeuPerfilProps> = ({...props}) =>{
 
-function MeuPerfil(){
-
-    const [editar, setEditar] = useState<boolean>(false)
-
-    const eventoClick = (prop: boolean) =>{
-        setEditar(prop)
-    }
 
     return(
         
 
         <ConteinerMaior>
             <Perfil 
-                onClick={() => eventoClick(false)} 
 
                 style = {{
-                    color: editar ? "black" : "white", 
-                    backgroundColor: editar ? Tema.colors.$cinzaNeutro : Tema.colors.$secundariaClara,
-                    borderRadius: editar ? "10px 0px 0px 10px" : "10px",
-                    border: editar ? "0px" :  "0.5px solid rgba(0, 0, 0, 0.5)"
+                    color: props.editar ? "black" : "white", 
+                    backgroundColor: props.editar ? Tema.colors.$cinzaNeutro : Tema.colors.$secundariaClara,
+                    borderRadius: props.editar ? "10px 0px 0px 10px" : "10px",
+                    border: props.editar ? "0px" :  "0.5px solid rgba(0, 0, 0, 0.5)"
                 }}>
                 <IconePerfil style={{ backgroundImage: `url(${MeuPerfilLogo})` }}></IconePerfil>
                 <TextoEscolha>Meu Perfil</TextoEscolha>
             </Perfil>
             <Editar 
-                onClick={() => eventoClick(true)} 
 
                 style = {{
-                    color: editar ? "white" : "black", 
-                    backgroundColor: editar ? Tema.colors.$secundariaClara : Tema.colors.$cinzaNeutro,
-                    borderRadius: editar ? "10px" : "0px 10px 10px 0px",
-                    border: editar ? "0.5px solid rgba(0, 0, 0, 0.5)" : "0px"
+                    color: props.editar ? "white" : "black", 
+                    backgroundColor: props.editar ? Tema.colors.$secundariaClara : Tema.colors.$cinzaNeutro,
+                    borderRadius: props.editar ? "10px" : "0px 10px 10px 0px",
+                    border: props.editar ? "0.5px solid rgba(0, 0, 0, 0.5)" : "0px"
                 }}>
 
                 <TextoEscolha>Editar e alterar dados do perfil</TextoEscolha>
