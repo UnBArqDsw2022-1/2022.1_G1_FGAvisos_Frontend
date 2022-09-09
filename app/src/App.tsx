304ad4c634bd39ddb112ce121afa12e6f22edc5a
LamequeFernandes <lamequesao@gmail.com>
2022-09-08 22:27:43 -0300

    componente de navbar implementado.
    Co-Authored-by: AntonioRangelC <antonio.rangel.02@gmail.com>

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import BarraNavegacao from './components/BarraNavegacao';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- app/src/components/BarraNavegacao/index.tsx
+++ app/src/components/BarraNavegacao/index.tsx
@@ -1,27 +1,24 @@
-import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../../assets/FGAvisosLogoClara.svg'
-import { Navbar, Nav} from 'react-bootstrap';
-import { FiLogOut } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 
-const BarraNavegacao = () => {
-    return (
-        <Navbar expand="lg" variant="dark" fixed="top">
-          <Navbar.Brand style={{ marginLeft: 25 }}>
-            <img src={Logo} alt="Logo" height={50} width={120} />
-          </Navbar.Brand>
-              <Navbar.Collapse id="navbar">
-                <Nav className="ml-auto">
-                  <Nav.Link as={Link} to="/">
-                    Home
-                  </Nav.Link>
-                  <Navbar.Brand as={Link} to="/login">
-                    <FiLogOut />
-                  </Navbar.Brand>
-                </Nav>
-              </Navbar.Collapse>
-        </Navbar>
-      );
-    }
+
+function BarraNavegacao() {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <img src={Logo} alt="Logo" height={50} width={120} />
+          <Nav className="end">
+            <Nav.Link href="#home">Avisos</Nav.Link>
+            <Nav.Link href="#features">Turmas</Nav.Link>
+            <Nav.Link href="#pricing">Perfil</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
 
 export default BarraNavegacao;
