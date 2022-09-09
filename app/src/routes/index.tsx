59ad048b490b0b891e04bc05cc33b42372ff4592
Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>
2022-09-09 13:02:02 -0300

    Adicionando as rotas iniciais das paginas
    Signed-off-by: Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>

---
+++ app/src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import LoginScreen from '../pages/PaginaLogin';
+import Perfil from '../pages/PaginaPerfil';
+import BarraNavegacao from '../components/BarraNavegacao';
+
+const Rotas = () => {
+    return (
+        <BrowserRouter>
+            <BarraNavegacao/>
+            <Routes>
+                <Route path="/login" element={<LoginScreen />} />
+                <Route path="/perfil" element={<Perfil />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Rotas;
