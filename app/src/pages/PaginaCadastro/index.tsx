96c4b4eb21b69a7c0f02d2c2e1ad8c16084f3e0f
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-09-09 11:45:37 -0300

    Adicionando página de cadastro

---
+++ app/src/pages/PaginaCadastro/index.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+        Corpo, 
+        CaixaCadastro, 
+        CampoConfirmarSenha, 
+        CampoSenha, 
+        CampoEmail, 
+        CampoUsuario,
+        SelecaoUsuario
+        } from './styles';
+import Input from '../../components/Input';
+import Tema from '../../utils/Tema';
+import DropdownComponent from '../../components/Dropdown';
+import Button from '../../components/Button';
+
+function PaginaCadastro(){
+
+    return(
+        <Corpo>
+            <CaixaCadastro>
+                <CampoUsuario>
+                    <Input 
+                        text={"Usuário"} 
+                        backgroundColor={Tema.colors.$secundariaClara} 
+                        colorFont={"white"}
+                        width={"493px"}
+                    ></Input>
+                </CampoUsuario>
+                
+                <CampoEmail>
+                    <Input 
+                        text={"Email"} 
+                        backgroundColor={Tema.colors.$secundariaClara} 
+                        colorFont={"white"}
+                        width={"493px"}
+                    ></Input>
+                </CampoEmail>
+                
+                <CampoSenha>
+                    <Input 
+                        text={"Senha"} 
+                        backgroundColor={Tema.colors.$secundariaClara} 
+                        colorFont={"white"}
+                        width={"493px"}
+                        type={"password"}
+                    ></Input>
+                </CampoSenha>
+
+                
+                <CampoConfirmarSenha>
+                    <Input 
+                        text={"Confirmar senha"} 
+                        backgroundColor={Tema.colors.$secundariaClara} 
+                        colorFont={"white"}
+                        width={"493px"}
+                        type={"password"}
+                    ></Input>
+                </CampoConfirmarSenha>
+
+                <SelecaoUsuario>
+                    <DropdownComponent
+                        optionList={["Professor", "Aluno", "Coordenador"]}
+                        value={"Selecione o tipo de usuário"}
+                        width={"493px"}
+                        heigth={"54px"}
+                    ></DropdownComponent>
+                </SelecaoUsuario>
+                
+                
+
+                <Button
+                    text={"Cadastrar"}
+                    backgroundColor={Tema.colors.$secundariaClara}
+                    colorFont={"white"}
+                    width = {203}
+                ></Button>
+
+            </CaixaCadastro>
+        </Corpo>
+
+    );
+
+}
+
+export default PaginaCadastro;
---
+++ app/src/pages/PaginaCadastro/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import {Tema} from '../../utils/Tema';
+
+
+export const Corpo = styled.body`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+export const CaixaCadastro = styled.div`
+    height: 500px;
+    width: 630px;
+    background-color: ${Tema.colors.$primariaEscura};
+    border: 0.5px;
+    border-radius: 7px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+
+`;
+
+export const CampoUsuario = styled.div`
+    height: 54px;
+    width: 493px;
+    display: flex;
+`;
+
+export const CampoEmail = styled.div`
+    height: 54px;
+    width: 493px;
+    display: flex;
+`;
+
+export const CampoSenha = styled.div`
+    height: 54px;
+    width: 493px;
+    display: flex;
+`;
+
+export const CampoConfirmarSenha = styled.div`
+    height: 54px;
+    width: 493px;
+    display: flex;
+`;
+
+export const SelecaoUsuario = styled.div`
+    height: 54px;
+    width: 493px;
+    display: inline;
+    margin: 15px 0;
+`;
+
+
