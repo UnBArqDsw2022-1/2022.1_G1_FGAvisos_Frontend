2a193f73a6b7efbafc2b8b61a03a6eb356450609
Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>
2022-09-09 14:59:16 -0300

    Iniciando os serviços
    Signed-off-by: Davi Marinho <davi-marinho@Davi-Marinho-PC.localdomain>

---
+++ app/src/services/Axios/avisoService.ts
@@ -0,0 +1,20 @@
+import { BASE_API } from './baseService';
+
+export async function login(
+    username: string, 
+    password: string 
+) {
+    try {
+        const response = await BASE_API.post('turma/', {
+            username: username,
+            password: password
+        });
+        return response;
+    } catch (error: any) {
+        if (error.response.status !== 401) {
+            alert('Não foi possivel realizar o Login.');
+        }
+        console.error(`An unexpected error ocourred while creating a new client.${error}`);
+    }
+    return false;
+}
